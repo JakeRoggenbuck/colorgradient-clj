9a184ee72b8d9531ef8262c37012aff1881897a1
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2022-09-06 22:11:23 -0700

    Add more functions and tests

--- src/colorgradient_clj/core.clj
+++ src/colorgradient_clj/core.clj
@@ -1,8 +1,14 @@
 (ns colorgradient-clj.core)
 
+(defn roughly-equal [a b]
+  (and (< -0.0000000001 (- a b)) (< (- a b) 0.0000000001)))
+
 (defn find-slope [x1 y1 x2 y2]
   (/ (- y2 y1) (- x2 x1)))
 
 (defn trunc [x]
   (+ 0.0 (with-precision 1 :rounding DOWN
     (* 1M (bigdec x)))))
+
+(defn fract [x]
+  (- x (trunc x)))
--- test/colorgradient_clj/core_test.clj
+++ test/colorgradient_clj/core_test.clj
@@ -2,6 +2,10 @@
   (:require [clojure.test :refer :all]
             [colorgradient-clj.core :refer :all]))
 
+(deftest roughly-equal-test
+  (testing "Pass one"
+    (is (roughly-equal 0.12300000000000022 0.123))))
+
 (deftest find-slope-test
   (testing "Pass one"
     (is (= 1.0 (find-slope 1.0 2.0 3.0 4.0)))
@@ -9,7 +13,10 @@
     (is (= 0.75 (find-slope 0.0 2.0 4.0 5.0)))
     (is (= 0.3333333333333333 (find-slope -2.0 1.0 1.0 2.0)))))
 
-
 (deftest trunc-test
   (testing "Pass one"
     (is (= 1.0 (trunc 1.3442)))))
+
+(deftest fract-test
+  (testing "Pass one"
+    (is (= 0.12300000000000022 (fract 2.123)))))
